c4adf54e7d220c95686bf1d4c05f4fc58134d1c1
SMFSW <SMFSW@users.noreply.github.com>
2015-10-25 23:20:01 +0100

    - LCD + Keypad + XBEE test added

---
+++ LiquidCrystal/SainSmart_LCDKeypad_XBEE/Keypad.ino
@@ -0,0 +1,44 @@
+
+//#define		V11		// Switch to V1.1 Thresholds
+
+// define some values used by the panel and buttons
+//static int lcd_key = 0;
+static int adc_key_in = 0;
+ 
+#define btnRIGHT	0
+#define btnUP		1
+#define btnDOWN		2
+#define btnLEFT		3
+#define btnSELECT	4
+#define btnNONE		5
+
+
+int read_LCD_buttons(){				// read the buttons
+	adc_key_in = analogRead(0);		// read the value from the sensor 
+
+	// my buttons when read are centered at these valies: 0, 144, 329, 504, 741
+	// we add approx 50 to those values and check to see if we are close
+	// We make this the 1st option for speed reasons since it will be the most likely result
+
+	if (adc_key_in > 1000) return btnNONE; 
+
+#ifdef V11
+	// For V1.1 us this threshold
+	if (adc_key_in < 50)   return btnRIGHT;  
+	if (adc_key_in < 250)  return btnUP; 
+	if (adc_key_in < 450)  return btnDOWN; 
+	if (adc_key_in < 650)  return btnLEFT; 
+	if (adc_key_in < 850)  return btnSELECT;
+#else
+	// For V1.0 comment the other threshold and use the one below:
+	if (adc_key_in < 50)   return btnRIGHT;  
+	if (adc_key_in < 195)  return btnUP; 
+	if (adc_key_in < 380)  return btnDOWN; 
+	if (adc_key_in < 555)  return btnLEFT; 
+	if (adc_key_in < 790)  return btnSELECT;   
+#endif
+
+	return btnNONE;		// when all others fail, return this.
+}
+ 
+
---
+++ LiquidCrystal/SainSmart_LCDKeypad_XBEE/SainSmart_LCDKeypad_XBEE.ino
@@ -0,0 +1,95 @@
+/*
+This example is for Series 2 XBee
+Receives a ZB RX packet and sets a PWM value based on packet data.
+Error led is flashed if an unexpected packet is received
+*/
+
+#include <SeqTimer.h>
+#include <XBee.h>
+#include <LiquidCrystal.h>
+ 
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);           // select the pins used on the LCD panel
+SeqTimer timerLCD;
+
+XBee xbee = XBee();
+XBeeResponse response = XBeeResponse();
+// create reusable response objects for responses we expect to handle 
+ZBRxResponse rx = ZBRxResponse();
+ModemStatusResponse msr = ModemStatusResponse();
+
+
+int statusLed = 13;
+int errorLed = 13;
+int dataLed = 13;
+
+void flashLed(int pin, int times, int wait) {
+	for (int i = 0; i < times; i++)
+	{
+		digitalWrite(pin, HIGH);
+		delay(wait);
+		digitalWrite(pin, LOW);
+		
+		if (i + 1 < times) { delay(wait); }
+	}
+}
+
+
+
+void setup(){
+	lcd.begin(16, 2);		// start the library
+
+	pinMode(statusLed, OUTPUT);
+	pinMode(errorLed, OUTPUT);
+	pinMode(dataLed,  OUTPUT);
+
+	timerLCD.init(250);
+
+	// start serial
+	Serial.begin(9600);
+	xbee.begin(Serial);
+
+	flashLed(statusLed, 3, 50);
+}
+
+
+void loop()
+{
+	if (timerLCD.getTimer())	{ displayLCD(); }
+	
+	xbee.readPacket();
+
+	if (xbee.getResponse().isAvailable())	// got something
+	{
+		if (xbee.getResponse().getApiId() == ZB_RX_RESPONSE)	// got a zb rx packet
+		{
+			
+			// now fill our zb rx class
+			xbee.getResponse().getZBRxResponse(rx);
+			
+			if (rx.getOption() == ZB_PACKET_ACKNOWLEDGED)	{ flashLed(statusLed, 10, 10); }	// the sender got an ACK
+			else											{ flashLed(errorLed, 2, 20); }		// we got it (obviously) but sender didn't get an ACK
+			
+			// set dataLed PWM to value of the first byte in the data
+			analogWrite(dataLed, rx.getData(0));
+		}
+		else if (xbee.getResponse().getApiId() == MODEM_STATUS_RESPONSE)
+		{
+			xbee.getResponse().getModemStatusResponse(msr);
+			// the local XBee sends this response on certain events, like association/dissociation
+			
+			if (msr.getStatus() == ASSOCIATED) { flashLed(statusLed, 10, 10); }	// yay this is great.  flash led
+			else if (msr.getStatus() == DISASSOCIATED) { flashLed(errorLed, 10, 10); }	// this is awful.. flash led to show our discontent
+			else { flashLed(statusLed, 5, 10); }// another status
+		}
+		else
+		{
+			// not something we were expecting
+			flashLed(errorLed, 1, 25);    
+		}
+	}
+	else if (xbee.getResponse().isError())
+	{
+		/*nss*/Serial.print("Error reading packet.  Error code: ");  
+		/*nss*/Serial.println(xbee.getResponse().getErrorCode());
+	}
+}
