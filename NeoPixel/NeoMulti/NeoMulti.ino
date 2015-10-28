da1e823f9d17d51f6a88b286c427d9ed29522c84
SMFSW <SMFSW@users.noreply.github.com>
2015-10-28 21:16:38 +0100

    - Changed pins & refactor

--- NeoPixel/NeoMulti/NeoMulti.ino
+++ NeoPixel/NeoMulti/NeoMulti.ino
@@ -3,9 +3,9 @@
 
 // Define some NeoPatterns for the two rings and the stick
 //  as well as some completion routines
-NeoPatterns Ring1(12, 0, NEO_GRB + NEO_KHZ800, &Ring1Complete);
-NeoPatterns Ring2(12, 1, NEO_GRB + NEO_KHZ800, &Ring2Complete);
-NeoPatterns Ring3(12, 2, NEO_GRB + NEO_KHZ800, &Ring3Complete);
+NeoPatterns ledStrip1(12, 7, NEO_GRB + NEO_KHZ800, &ledStrip1Complete);
+NeoPatterns ledStrip2(12, 12, NEO_GRB + NEO_KHZ800, &ledStrip2Complete);
+NeoPatterns ledStrip3(12, 13, NEO_GRB + NEO_KHZ800, &ledStrip3Complete);
 
 // Initialize everything and prepare to start
 void setup()
@@ -16,54 +16,54 @@ void setup()
    pinMode(9, INPUT_PULLUP);
     
     // Initialize all the pixelStrips
-    Ring1.begin();
-    Ring2.begin();
-    Ring3.begin();
+    ledStrip1.begin();
+    ledStrip2.begin();
+    ledStrip3.begin();
     
     // Kick off a pattern
-    Ring1.TheaterChase(Ring1.Color(255,255,0), Ring1.Color(0,0,50), 100);
-    Ring2.RainbowCycle(3);
-    Ring2.Color1 = Ring1.Color1;
-    Ring3.Scanner(Ring1.Color(255,0,0), 55);
+    ledStrip1.TheaterChase(ledStrip1.Color(255,255,0), ledStrip1.Color(0,0,50), 100);
+    ledStrip2.RainbowCycle(3);
+    ledStrip2.Color1 = ledStrip1.Color1;
+    ledStrip3.Scanner(ledStrip1.Color(255,0,0), 55);
 }
 
 // Main loop
 void loop()
 {
     // Update the rings.
-    Ring1.Update();
-    Ring2.Update();    
+    ledStrip1.Update();
+    ledStrip2.Update();    
     
     // Switch patterns on a button press:
     if (digitalRead(8) == LOW) // Button #1 pressed
     {
-        // Switch Ring1 to FASE pattern
-        Ring1.ActivePattern = FADE;
-        Ring1.Interval = 20;
-        // Speed up the rainbow on Ring2
-        Ring2.Interval = 0;
+        // Switch ledStrip1 to FASE pattern
+        ledStrip1.ActivePattern = FADE;
+        ledStrip1.Interval = 20;
+        // Speed up the rainbow on ledStrip2
+        ledStrip2.Interval = 0;
         // Set stick to all red
-        Ring3.ColorSet(Ring3.Color(255, 0, 0));
+        ledStrip3.ColorSet(ledStrip3.Color(255, 0, 0));
     }
     else if (digitalRead(9) == LOW) // Button #2 pressed
     {
-        // Switch to alternating color wipes on Rings1 and 2
-        Ring1.ActivePattern = COLOR_WIPE;
-        Ring2.ActivePattern = COLOR_WIPE;
-        Ring2.TotalSteps = Ring2.numPixels();
+        // Switch to alternating color wipes on ledStrips1 and 2
+        ledStrip1.ActivePattern = COLOR_WIPE;
+        ledStrip2.ActivePattern = COLOR_WIPE;
+        ledStrip2.TotalSteps = ledStrip2.numPixels();
         // And update tbe stick
-        Ring3.Update();
+        ledStrip3.Update();
     }
     else // Back to normal operation
     {
         // Restore all pattern parameters to normal values
-        Ring1.ActivePattern = THEATER_CHASE;
-        Ring1.Interval = 100;
-        Ring2.ActivePattern = RAINBOW_CYCLE;
-        Ring2.TotalSteps = 255;
-        Ring2.Interval = min(10, Ring2.Interval);
+        ledStrip1.ActivePattern = THEATER_CHASE;
+        ledStrip1.Interval = 100;
+        ledStrip2.ActivePattern = RAINBOW_CYCLE;
+        ledStrip2.TotalSteps = 255;
+        ledStrip2.Interval = min(10, ledStrip2.Interval);
         // And update tbe stick
-        Ring3.Update();
+        ledStrip3.Update();
     }    
 }
 
@@ -71,42 +71,42 @@ void loop()
 //Completion Routines - get called on completion of a pattern
 //------------------------------------------------------------
 
-// Ring1 Completion Callback
-void Ring1Complete()
+// ledStrip1 Completion Callback
+void ledStrip1Complete()
 {
     if (digitalRead(9) == LOW)  // Button #2 pressed
     {
-        // Alternate color-wipe patterns with Ring2
-        Ring2.Interval = 40;
-        Ring1.Color1 = Ring1.Wheel(random(255));
-        Ring1.Interval = 20000;
+        // Alternate color-wipe patterns with ledStrip2
+        ledStrip2.Interval = 40;
+        ledStrip1.Color1 = ledStrip1.Wheel(random(255));
+        ledStrip1.Interval = 20000;
     }
     else  // Retrn to normal
     {
-      Ring1.Reverse();
+      ledStrip1.Reverse();
     }
 }
 
-// Ring 2 Completion Callback
-void Ring2Complete()
+// ledStrip 2 Completion Callback
+void ledStrip2Complete()
 {
     if (digitalRead(9) == LOW)  // Button #2 pressed
     {
-        // Alternate color-wipe patterns with Ring1
-        Ring1.Interval = 20;
-        Ring2.Color1 = Ring2.Wheel(random(255));
-        Ring2.Interval = 20000;
+        // Alternate color-wipe patterns with ledStrip1
+        ledStrip1.Interval = 20;
+        ledStrip2.Color1 = ledStrip2.Wheel(random(255));
+        ledStrip2.Interval = 20000;
     }
     else  // Retrn to normal
     {
-        Ring2.RainbowCycle(random(0,10));
+        ledStrip2.RainbowCycle(random(0,10));
     }
 }
 
 // Stick Completion Callback
-void Ring3Complete()
+void ledStrip3Complete()
 {
     // Random color change for next scan
-    Ring3.Color1 = Ring3.Wheel(random(255));
+    ledStrip3.Color1 = ledStrip3.Wheel(random(255));
 }
 
