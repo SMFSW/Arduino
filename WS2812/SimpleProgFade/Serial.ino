c08fc3e1f8e74c1f823a890736f69c09f71a7621
SMFSW <SMFSW@users.noreply.github.com>
2015-11-21 00:06:43 +0100

    - Fade all LED synchronously added - SCI module added (for testing/debug needs yet)

---
+++ WS2812/SimpleProgFade/Serial.ino
@@ -0,0 +1,82 @@
+
+#define DEF_TAILLE_SCI_IN    64      //!< Taille réservée pour le buffer d'entrée SCI
+
+static char        breakoutChar = ';';   //!< Declare ASCII value of tab as string breakout
+
+static String      SCIIn = "";       //!< Declare a new string 
+static boolean      SCIbreakout;      //!< Defines if an acquired SCI string breakout char is reached
+static int        SCIInNbChar = 0;    //!< Inutilisé, sert juste à compter le nombre de char recus
+static int       (*process)(String);
+
+void setupSerial()
+{
+  process = msgSerial;
+  
+  Serial.begin(57600);
+  
+  Serial.println(ProjectName);
+  Serial.print("Running software version: ");
+  Serial.println(SWVersion);
+  Serial.print("./>");
+}
+
+void taskSerial()
+{
+  if (SCIbreakout == true)
+  {
+    Serial.println(SCIIn);
+    Serial.print("$/>");
+    int rem = SCIIn.length();
+    
+    (void) msgSerial(SCIIn);
+    
+    // Clean treated part of serial internal buffer
+    SCIIn.remove(0, rem);
+    SCIbreakout = false;
+    
+    Serial.println("");
+    Serial.print("./>");
+  }
+}
+
+/*!
+**  \brief Called whenever a new data has been received on RX
+**  \remark This routine is run between each time loop() runs, no delay here set in here
+**  \note Multiple bytes of data may be available
+**/
+void serialEvent(void)
+{
+  if (Serial.available())
+  {
+    char inChar = (char) Serial.read();
+
+    SCIIn += inChar;
+
+    if ( (++SCIInNbChar > DEF_TAILLE_SCI_IN-1) || (inChar == breakoutChar) )
+    {
+      SCIbreakout = true;
+    }
+  }
+}
+
+/*!
+**  \brief write breakoutChar
+**
+**  \param[in] breakout - New ASCII digit as breakout
+**  \return nothing
+**/
+inline void setBreakoutChar(char breakout)
+{
+  breakoutChar = breakout;
+}
+
+/*!
+**  \brief reads breakoutChar
+**
+**  \return breakoutChar
+**/
+inline char getBreakoutChar()
+{
+  return (char) breakoutChar;
+}
+
--- WS2812/SimpleProgFade/SimpleProgFade.ino
+++ WS2812/SimpleProgFade/SimpleProgFade.ino
@@ -1,7 +1,9 @@
 #include <Adafruit_NeoPixel.h>
 
 //#define DBG_SERIAL
-#define DBG_SERIAL2
+
+String ProjectName = "Simple Progressive Fade demo - SMFSW";  //!< Project Name
+String SWVersion = "v0.2";  //!< Version en cours du soft
 
 #define	pinPB1	8
 #define	pinPB2	9
@@ -12,7 +14,7 @@
 
 enum	direction { FORWARD = 0, BACKWARD };
 enum	status { WAIT = 0, RESET, RUN };
-enum	mode { LINEAR_FADE_DIM = 0, NONE };
+enum	mode { NONE = 0, FADE_DIM_LINEAR, FADE_DIM_ALL };
 
 Adafruit_NeoPixel Strip = Adafruit_NeoPixel(NbLEDsStrip1, 6, NEO_GRB + NEO_KHZ800);
 
@@ -65,7 +67,7 @@ void setup()
 	StripState.fromtop = false;
 	StripState.valinc = 3;
 
-	StripState.Mode = LINEAR_FADE_DIM;
+	StripState.Mode = FADE_DIM_LINEAR;
 	for (int i = 0 ; i < sizeof(StripState.DimLED) ; i++) { StripState.DimLED[i] = 0; }
 
 	// Initialize all the pixelStrips
@@ -74,10 +76,8 @@ void setup()
 	// Initialize push buttons
 	pinMode(pinPB1, INPUT_PULLUP);
 	pinMode(pinPB2, INPUT_PULLUP);
-	
-	#if DBG_SERIAL
-	Serial.begin(9600);
-	#endif
+
+  setupSerial();
 }
 
 // Main loop
@@ -113,9 +113,11 @@ void loop()
 		memoBP2 = false;
 		timeBP2 = millis();
 	}
+
+	taskSerial();
 }
 
-void StripHandle()
+boolean StripHandle()
 {
 	uint16_t acttime = millis();
 	boolean show = false;
@@ -136,65 +138,85 @@ void StripHandle()
 		for (int i = 0 ; i < Strip.numPixels() ; i++)
 		{
 			Strip.setPixelColor(i, DimColor(StripState.color, StripState.DimLED[StripState.fromtop ? sizeof(StripState.DimLED) - 1 - i : i]));
-			
-			#if DBG_SERIAL
-			Serial.print(StripState.DimLED[i]);
-			Serial.print("\t");
-			#endif
 		}
-		#if DBG_SERIAL
-		Serial.print("\n");
-		#endif
 		Strip.show();
 	}
+	
+	return show;
 }
 
 void UpdateDimTab()
 {
 	switch (StripState.Mode)
 	{
-		case LINEAR_FADE_DIM:
+		case FADE_DIM_LINEAR:
 		{
-			if (StripState.Dir == FORWARD)
+			for (int i = 0 ; i < sizeof(StripState.DimLED) ; i++)
 			{
-				StripState.idx++;
+				uint8_t NumLED = StripState.Dir ? sizeof(StripState.DimLED) - 1 - i : i;
+				int8_t Inc = StripState.Dir ? -StripState.valinc : StripState.valinc;
+				int16_t Val = max(0, min(255, StripState.DimLED[NumLED] + Inc));
 				
-				for (int i = 0 ; i < sizeof(StripState.DimLED) ; i++)
+				if (StripState.Dir == FORWARD)
 				{
-					uint8_t NumLED = sizeof(StripState.DimLED) - 1 - i;
-					uint16_t Val = min(255, StripState.DimLED[NumLED] + StripState.valinc);
-
+					StripState.idx++;
+					
 					if (StripState.DimLED[NbLEDsStrip1-1] == 255)
 					{
 						StripState.Status = WAIT;
 						StripState.Dir = BACKWARD;
 					}
-					else if (	(NumLED == 0)
-							 ||	(StripState.DimLED[NumLED-1] > DELTA_LED))
+					else if ( (NumLED == 0) || (StripState.DimLED[NumLED-1] > DELTA_LED) )
 					{
 						if (Val <= 255)	StripState.DimLED[NumLED] = Val;
 					}
 				}
-			}
-			else
-			{
-				StripState.idx--;
-				
-				for (int i = 0 ; i < sizeof(StripState.DimLED) ; i++)
+				else
 				{
-					int16_t Val = max(0, StripState.DimLED[i] - StripState.valinc);
+					StripState.idx--;
 					
 					if (StripState.DimLED[0] == 0)
 					{
 						StripState.Status = WAIT;
 						StripState.Dir = FORWARD;
 					}
-					else if (	(i == sizeof(StripState.DimLED)-1)
-							 ||	(StripState.DimLED[i+1] < 255 - DELTA_LED))
+					else if ( (NumLED == sizeof(StripState.DimLED)-1) || (StripState.DimLED[NumLED+1] < 255 - DELTA_LED) )
 					{
-						if (Val >= 0)	StripState.DimLED[i] = Val;
+						if (Val >= 0)	StripState.DimLED[NumLED] = Val;
 					}
 				}
+			}
+		}
+		break;
+
+		case FADE_DIM_ALL:
+		{
+			boolean setWait = true;
+			
+			for (int i = 0 ; i < sizeof(StripState.DimLED) ; i++)
+			{
+				uint8_t NumLED = StripState.Dir ? sizeof(StripState.DimLED) - 1 - i : i;
+				int8_t Inc = StripState.Dir ? -StripState.valinc : StripState.valinc;
+				int16_t Val = max(0, min(255, StripState.DimLED[NumLED] + Inc));
+
+				if (StripState.Dir == FORWARD)
+				{
+					StripState.idx++;
+					if (Val <= 255)						{ StripState.DimLED[NumLED] = Val; }
+					if (StripState.DimLED[i] != 255)	{ setWait = false; }
+				}
+				else
+				{
+					StripState.idx--;
+					if (Val >= 0)					{ StripState.DimLED[NumLED] = Val; }
+					if (StripState.DimLED[i] != 0)	{ setWait = false; }
+				}
+			}
+
+			if (setWait)
+			{
+				StripState.Status = WAIT;
+				StripState.Dir = BACKWARD;
 			}
 		}
 		break;
